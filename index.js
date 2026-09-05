ce34cb4451fa0053f0cfedfd2a309e577aa93709
peniel001 <penielcrypt@gmail.com>
2026-09-06 00:43:07 +0100

    Initial commit - tasks API with CORS

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+import cors from "cors"
 import express from 'express'
 import taskRouter from './routes/taskRoutes.js'
 import userRouter from './routes/userRoutes.js'
 
 const app = express()
-const PORT = 3000
+app.use(cors())
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json())
 app.use('/api/v1/tasks', taskRouter)
